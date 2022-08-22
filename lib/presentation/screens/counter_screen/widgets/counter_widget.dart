da1da6da8862550b9e68e2a42ad35f35c79f05c1
LordOlumide <olumideolubosede@yahoo.com>
2022-08-22 22:18:04 +0100

    fading animation added

--- lib/presentation/screens/counter_screen/widgets/counter_widget.dart
+++ lib/presentation/screens/counter_screen/widgets/counter_widget.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:bloc_architecture_app/logic/cubit/counter_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:animate_do/animate_do.dart';
 
-class CounterContainer extends StatelessWidget {
+class CounterContainer extends StatefulWidget {
   const CounterContainer({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<CounterContainer> createState() => _CounterContainerState();
+}
+
+class _CounterContainerState extends State<CounterContainer>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    _animationController.forward(from: 0.0);
+
     return Container(
       width: 200,
       height: 200,
@@ -17,15 +43,19 @@ class CounterContainer extends StatelessWidget {
         shape: BoxShape.circle,
       ),
       child: Center(
-        child: Text(
-          context
-              .select<CounterCubit, CounterState>((cubit) => cubit.state)
-              .counterValue
-              .toString(),
-          style: Theme.of(context).textTheme.headline1!.copyWith(
-                fontWeight: FontWeight.bold,
-                fontSize: 130,
-              ),
+        child: ZoomIn(
+          controller: (controller) => _animationController = controller,
+          manualTrigger: true,
+          child: Text(
+            context
+                .select<CounterCubit, CounterState>((cubit) => cubit.state)
+                .counterValue
+                .toString(),
+            style: Theme.of(context).textTheme.headline1!.copyWith(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 100,
+                ),
+          ),
         ),
       ),
     );
